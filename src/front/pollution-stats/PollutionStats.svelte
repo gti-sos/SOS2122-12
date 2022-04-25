1976401d845b2b17b388740bed88b700f4aa047e
fravaralg <fravaralg@alum.us.es>
2022-04-25 09:48:37 +0200

    f08

--- src/front/pollution-stats/PollutionStats.svelte
+++ src/front/pollution-stats/PollutionStats.svelte
@@ -12,8 +12,9 @@
     let updatedPlasticWaste;
     let updatedGaseousWaste;
     let updatedCollectedWaste;
-
+    
     onMount(getPollutionStats);
+    
     async function getPollutionStats(){
         console.log("Fetching pollutions....");
         const res = await fetch("/api/v1/pollution-stats/"+params.country+"/"+params.year); 
