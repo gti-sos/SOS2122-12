68251f916477198aad9e96833efb7bb02193c709
fravaralg <fravaralg@alum.us.es>
2022-05-29 10:44:41 +0200

    cambios

--- src/front/Integration.svelte
+++ src/front/Integration.svelte
@@ -6,8 +6,8 @@
         <ul>
             <li> Javier Vargas Algaba(Contaminación ambiental):
                 <ul>
-                    <li><a href="/#/pollution-stats_graph_population">Integración niveles de poblacion</a></li>
-                    <li><a href="/#/pollution-stats_graph_inmigrant">Integración inmigrantes</a></li>
+                    <li><a href="/#/pollution-stats_population">Integración niveles de poblacion</a></li>
+                    <li><a href="/#/pollution-stats_inmigrant">Integración inmigrantes</a></li>
                     <li><a href="/#/pollution-stats_vacuna">Gráfica externa 1</a></li>
                     <li><a href="/#/pollution-stats_sport">Gráfica externa 2</a></li>
                 </ul>
